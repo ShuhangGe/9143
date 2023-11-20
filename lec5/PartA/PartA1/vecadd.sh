6e3fe0c0a4c6827d18674d257e14c8f60d2df9e4
ShuhangGe <sg7484@nyu.edu>
2023-11-20 14:54:19 -0500

    qusetion2

--- lec5/PartA/PartA1/vecadd.sh
+++ lec5/PartA/PartA1/vecadd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash  
 
-for num in {500..1000..2000}:
-do
-    echo $num
-    ./vecadd00 $num
-    sleep 10
-    ./vecadd01 $num
-done
+# for num in {500..1000..2000}
+# do
+#     echo $num
+#     ./vecadd00 $num
+
+./vecadd00 500
+./vecadd01 500
+./vecadd00 1000
+./vecadd01 1000
+./vecadd00 2000
+./vecadd01 2000
+
