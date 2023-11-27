aa88b63238f3952b387cef9cb8028e3248943b44
ShuhangGe <sg7484@nyu.edu>
2023-11-26 19:08:51 -0500

    add

---
+++ lec5/PartC/C3.cu
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cudnn.h>
+
+// Error checking macro for CUDA and cuDNN calls
+#define checkCUDNN(expression)                                 \
+{                                                              \
+  cudnnStatus_t status = (expression);                         \
+  if (status != CUDNN_STATUS_SUCCESS) {                        \
+    std::cerr << "Error on line " << __LINE__ << ": "          \
+              << cudnnGetErrorString(status) << std::endl;     \
+    std::exit(EXIT_FAILURE);                                   \
+  }                                                            \
+}
+
+int main() {
+    cudnnHandle_t cudnn;
+    checkCUDNN(cudnnCreate(&cudnn));
+
+    // Define tensor dimensions and data types here
+    // For example, let's define some arbitrary dimensions
+    int batch_size = 1, channels = 3, height = 128, width = 128;
+    int filter_height = 3, filter_width = 3, output_channels = 10;
+
+    // Create and set tensor descriptors
+    cudnnTensorDescriptor_t input_descriptor;
+    cudnnFilterDescriptor_t filter_descriptor;
+    cudnnTensorDescriptor_t output_descriptor;
+    cudnnConvolutionDescriptor_t convolution_descriptor;
+
+    checkCUDNN(cudnnCreateTensorDescriptor(&input_descriptor));
+    checkCUDNN(cudnnSetTensor4dDescriptor(input_descriptor, 
+                                          CUDNN_TENSOR_NCHW, 
+                                          CUDNN_DATA_FLOAT, 
+                                          batch_size, channels, height, width));
+
+    checkCUDNN(cudnnCreateFilterDescriptor(&filter_descriptor));
+    checkCUDNN(cudnnSetFilter4dDescriptor(filter_descriptor, 
+                                          CUDNN_DATA_FLOAT, 
+                                          CUDNN_TENSOR_NCHW, 
+                                          output_channels, channels, filter_height, filter_width));
+
+    checkCUDNN(cudnnCreateTensorDescriptor(&output_descriptor));
+
+    checkCUDNN(cudnnCreateConvolutionDescriptor(&convolution_descriptor));
+    checkCUDNN(cudnnSetConvolution2dDescriptor(convolution_descriptor, 
+                                               1, 1, 1, 1, 1, 1, 
+                                               CUDNN_CONVOLUTION, CUDNN_DATA_FLOAT));
+
+    // Find the dimensions of the convolution output
+    int n, c, h, w;
+    checkCUDNN(cudnnGetConvolution2dForwardOutputDim(convolution_descriptor, 
+                                                     input_descriptor, 
+                                                     filter_descriptor, 
+                                                     &n, &c, &h, &w));
+
+    checkCUDNN(cudnnSetTensor4dDescriptor(output_descriptor, 
+                                          CUDNN_TENSOR_NCHW, 
+                                          CUDNN_DATA_FLOAT, 
+                                          n, c, h, w));
+
+    // Allocate memory for input, filter, and output
+    float *input, *filter, *output;
+    cudaMalloc(&input, batch_size * channels * height * width * sizeof(float));
+    cudaMalloc(&filter, output_channels * channels * filter_height * filter_width * sizeof(float));
+    cudaMalloc(&output, n * c * h * w * sizeof(float));
+
+    // Initialize memory - omitted for brevity
+
+    // Choose the fastest convolution algorithm
+    cudnnConvolutionFwdAlgo_t convolution_algorithm;
+    checkCUDNN(cudnnGetConvolutionForwardAlgorithm(cudnn, 
+                                                   input_descriptor, 
+                                                   filter_descriptor,
+                                                   convolution_descriptor, 
+                                                   output_descriptor,
+                                                   CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 
+                                                   0, 
+                                                   &convolution_algorithm));
+
+    // Allocate workspace for the convolution
+    size_t workspace_bytes = 0;
+    checkCUDNN(cudnnGetConvolutionForwardWorkspaceSize(cudnn, 
+                                                       input_descriptor, 
+                                                       filter_descriptor,
+                                                       convolution_descriptor, 
+                                                       output_descriptor, 
+                                                       convolution_algorithm, 
+                                                       &workspace_bytes));
+
+    void *workspace;
+    cudaMalloc(&workspace, workspace_bytes);
+
+    // Perform the convolution
+    float alpha = 1.0f, beta = 0.0f;
+    checkCUDNN(cudnnConvolutionForward(cudnn, 
+                                       &alpha, 
+                                       input_descriptor, 
+                                       input, 
+                                       filter_descriptor, 
+                                       filter, 
+                                       convolution_descriptor, 
+                                       convolution_algorithm, 
+                                       workspace, 
+                                       workspace_bytes, 
+                                       &beta, 
+                                       output_descriptor, 
+                                       output));
+
+    // Compute checksum
+    // ...
+
+    // Cleanup
+    cudaFree(input);
+    cudaFree(filter);
+    cudaFree(output);
+    cudaFree(workspace);
+    cudnnDestroyTensorDescriptor(input_descriptor);
+    cudnnDestroyTensorDescriptor(output_descriptor);
+    cudnnDestroyFilterDescriptor(filter_descriptor);
+    cudnnDestroyConvolutionDescriptor(convolution_descriptor);
+    cudnnDestroy(cudnn);
+
+    return 0;
+}
